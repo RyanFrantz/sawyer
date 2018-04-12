24d02a755819d14a66d1e96ba7886fb23b1220a6
Ryan Frantz <rfrantz1@bloomberg.net>
2018-04-12 16:20:52 -0400

    RDTICHEF-1258: Adds support for defining any metric type.
    - Allows a user to override the default behavior of shipping a counter with a sample rate of 1.0.
    - Folks can ship a counter with a sample rate of 0.5, for example. Or a gauge.
    - They can ship a metric with tags, etc.
    - Updates the behavior of the runner and parser classes.
    - Updates the behavior of the aggrocrag publisher.
    - Fixes up tests, including an unrelated bug that would only be found when running a test a second time (because the offset file was written).
    - This will still need some in-production validation to ensure metrics are emitted to aggrocrag/GUTS.

--- lib/sawyer/exceptions.rb
+++ lib/sawyer/exceptions.rb
@@ -1,9 +1,7 @@
 module Sawyer
-
   class ParserNotFound < RuntimeError
   end
 
   class LogfileNotFound < RuntimeError
   end
-
 end
--- spec/config_derived_parser_spec.rb
+++ spec/config_derived_parser_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
+# Here we test a complex example of a config-derived parser whose attributes
+# are explicitly defined.
+# For a general regexp-to-string parser example, see spec/parser_spec.rb.
 describe 'An example parser' do
   let(:runner) {Sawyer::Runner.new}
   before(:each) do
     @logfile     = "#{File.dirname(__FILE__)}/fixtures/files/client.log"
-    @offset_file = "/tmp/#{File.basename(@logfile)}.offset"
+    # Create a unique offset file so we don't get stuck with a previous offset
+    # file gumming up the works during lots of manual testing.
+    @offset_file = "/tmp/#{File.basename(@logfile)}-#{Time.now.to_i}.offset"
     argv = ['exceptions_spec.rb'] # ARGV[0] is program name.
-    argv << %w(--parser example_parser)
+    argv << %w(--parser chef_client_log)
     argv << ["--log-file", @logfile]
     argv << ["--offset-file", @offset_file]
     argv << ["--config-file", "sawyer.yml.example"]
@@ -15,11 +20,18 @@ describe 'An example parser' do
   end
 
   context 'defined via the config' do
-    it "instantiates a Sawyer::Parser::ExampleParser object" do
-      puts runner.inspect
-      expect(runner.parser).to be_an_instance_of(Sawyer::Parser::ExampleParser)
+    it "instantiates a Sawyer::Parser::ChefClientLog object" do
+      #puts runner.inspect
+      expect(runner.parser).to be_an_instance_of(Sawyer::Parser::ChefClientLog)
       expect(runner.parser.logfile).to eq(@logfile)
       expect(runner.parser.offset_file).to eq(@offset_file)
+      runner.parser.parse
+      expect(runner.parser.metrics['sawyer.info']['value']).to eq(35)
+      expect(runner.parser.metrics['sawyer.info']['type']).to eq('c')
+      expect(runner.parser.metrics['sawyer.info']['sample_rate']).to eq('0.1')
+      expect(runner.parser.metrics['sawyer.warn']['value']).to eq(12)
+      expect(runner.parser.metrics['sawyer.warn']['type']).to eq('c')
+      expect(runner.parser.metrics['sawyer.warn']['sample_rate']).to eq('0.3')
     end
   end
 
